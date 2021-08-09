5201699f969a9fa54297660254b5d9c3221775d4
Vincenzo Iannucci <34918941+enzo2605@users.noreply.github.com>
2021-08-09 11:47:33 +0200

    update

---
+++ 1641. Count Sorted Vowel Strings/main.cpp
@@ -0,0 +1,24 @@
+/** Creato da Vincenzo Iannucci in data 04/03/2021 
+ * Dato un intero n, restituisce il numero di stringhe di lunghezza n che consistono solo di vocali (a, e, i, o, u) 
+ * e sono ordinate lessicograficamente. 
+ * Una stringa s è ordinata lessicograficamente se per tutti i validi i, s [i] è uguale o viene prima 
+ * di s [i + 1] nell'alfabeto.
+ * **/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int countVowelStrings(int n) {
+        return 0;
+    }
+};
+
+
+int main() {
+    Solution s;
+    int n = 2;
+    cout << s.countVowelStrings(n) << endl;
+    return 0;
+}
